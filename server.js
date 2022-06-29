45b33bf8bfd2c05a4563b866fd6fd3b1e8f935b1
cheddar <nowaczewskijack@gmail.com>
2022-06-28 23:57:30 -0400

    add server.js

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+const mysql = require("mysql2");
 const express = require("express");
 
 const PORT = process.env.PORT || 3001;
@@ -7,6 +8,24 @@ const app = express();
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
+// Connect to database
+const db = mysql.createConnection(
+  {
+    host: "localhost",
+    // Your MySQL username,
+    user: "root",
+    // Your MySQL password
+    password: "",
+    database: "election",
+  },
+  console.log("Connected to the election database.")
+);
+
+db.query(`SELECT * FROM candidates`, (err, rows) => {
+  console.log(rows);
+});
+// Connect to database
+
 // Default response for any other request (NOT FOUND)
 app.use((req, res) => {
   res.status(404).end();
@@ -14,5 +33,5 @@ app.use((req, res) => {
 
 // function that will start the express js server on port 3001
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT},  http://localhost:3001/`);
+  console.log(`Server running on port ${PORT},  http://localhost:3001`);
 });
